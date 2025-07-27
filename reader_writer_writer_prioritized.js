a1d230c1c1873ed13815aca7ab24234e00b4b51f
kINo <kinobhu@outlook.com>
2025-07-27 20:49:52 +0800

    Add reader-writer(writer prioritized) demo

---
+++ reader_writer_writer_prioritized.js
@@ -0,0 +1,93 @@
+import {
+	Frame, Scheduler,
+} from './conc.js';
+
+import {
+	spin_lock_yld, spin_unlock,
+} from './spinlock.js'
+
+import {
+	Mutex, mutex_lock, mutex_unlock
+} from './mutex.js';
+
+
+const count = 20;
+const s_count = 21;
+const m_rw = new Mutex(0, 1, 2, 3);
+const m_wr = new Mutex(10, 11, 12, 13);
+
+const reader = tid => [
+	...mutex_lock(m_wr, tid, 0, 1),
+	
+	...spin_lock_yld(s_count, 0),
+	
+	`lod  2, ${count}`,
+	`btr  2, r${tid}_prw_over`,
+	
+	...mutex_lock(m_rw, tid, 0, 1),
+	
+	`lab  r${tid}_prw_over`,
+	`adi  2, +1`,
+	`sto  2, ${count}`,
+	
+	...spin_unlock(s_count, 0),
+	
+	...mutex_unlock(m_wr, 0, 1),
+	
+	/* Reader's work */
+	`imm  3, 10`,
+	`prs  Reading(${tid})...`,
+	`adi  3, -1`,
+	`btr  3, :-2`,
+	
+	...spin_lock_yld(s_count, 0),
+	
+	`lod  2, ${count}`,
+	`adi  2, -1`,
+	`sto  2, ${count}`,
+	`btr  2, r${tid}_vrw_over`,
+	
+	...mutex_unlock(m_rw, 0, 1),
+	
+	`lab  r${tid}_vrw_over`,
+	...spin_unlock(s_count, 0),
+];
+
+const writer = tid => [
+	...mutex_lock(m_wr, tid, 0, 1),
+	
+	...mutex_lock(m_rw, tid, 0, 1),
+	
+	/* Writer's work */
+	`imm  3, 20`,
+	`prs  Writing(${tid})...`,
+	`adi  3, -1`,
+	`btr  3, :-2`,
+	
+	...mutex_unlock(m_rw, 0, 1),
+	
+	...mutex_unlock(m_wr, 0, 1),
+];
+
+new Scheduler(
+{
+	'0': {
+		frame: new Frame,
+		cmds: [ ...reader(0) ]
+	},
+	'1': {
+		frame: new Frame,
+		cmds: [ ...writer(1) ]
+	},
+	'2': {
+		frame: new Frame,
+		cmds: [ ...reader(2) ]
+	},
+},
+o => {
+	o.memory[2]  = o.memory[3]  = 4;
+	o.memory[12] = o.memory[13] = 14;
+}
+).loop();
+
+/* writer prioritized */
