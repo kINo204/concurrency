8a4f36d294c57cc5fe4373b2bc294f4c4cb24061
kINo <kinobhu@outlook.com>
2025-09-26 03:23:17 +0800

    Mod producer-consumer demo

--- producer_consumer.js
+++ producer_consumer.js
@@ -7,67 +7,46 @@ import {
 } from './mutex.js';
 
 import {
-	Semaphore, sem_post, sem_wait,
+	Semaphore, sem_post, sem_show, sem_wait,
 } from './semaphore.js';
 
-const mtx = new Mutex(20);
 const nobjects = new Semaphore(0);
 const nblanks  = new Semaphore(10);
+const mtx = new Mutex(20);
 
+const producer = tid => ({
+	frame: new Frame(),
+	cmds: [
+		...sem_wait(nblanks, tid, 0, 1),
+		...mutex_lock(mtx, tid, 0, 1),
+
+		`prs  producer_${tid}`,
+
+		...mutex_unlock(mtx, 0, 1),
+		...sem_post(nobjects, 0, 1),
+	]
+})
+
+const consumer = tid => ({
+	frame: new Frame(),
+	cmds: [
+		...sem_wait(nobjects, tid, 0, 1),
+		...mutex_lock(mtx, tid, 0, 1),
+
+		`prs  consumer_${tid}`,
+
+		...mutex_unlock(mtx, 0, 1),
+		...sem_post(nblanks, 0, 1),
+	]
+})
+
+const [M, N] = [5, 7];
+const threads = Array.from({length: M}, (_, i)=>consumer(i))
+				.concat(Array.from({length: N}, (_, i)=>producer(i + M)));
 new Scheduler(
-{
-	'0': {
-		frame: new Frame(),
-		cmds: [
-			...sem_wait(nobjects, 0, 0, 1),
-			
-			...mutex_lock(mtx, 1, 0, 1),
-			'prs  consumer_1',
-			...mutex_unlock(mtx, 0, 1),
-			
-			...sem_post(nblanks, 0, 1),
-		]
-	},
-	'1': {
-		frame: new Frame(),
-		cmds: [
-			...sem_wait(nobjects, 1, 0, 1),
-			
-			...mutex_lock(mtx, 1, 0, 1),
-			'prs  consumer_2',
-			...mutex_unlock(mtx, 0, 1),
-			
-			...sem_post(nblanks, 0, 1),
-		]
-	},
-	'2': {
-		frame: new Frame(),
-		cmds: [
-			...sem_wait(nblanks, 2, 0, 1),
-			
-			...mutex_lock(mtx, 0, 0, 1),
-			'prs  producer_1',
-			...mutex_unlock(mtx, 0, 1),
-			
-			...sem_post(nobjects, 0, 1),
-		]
-	},
-	'3': {
-		frame: new Frame(),
-		cmds: [
-			...sem_wait(nblanks, 3, 0, 1),
-			
-			...mutex_lock(mtx, 0, 0, 1),
-			'prs  producer_2',
-			...mutex_unlock(mtx, 0, 1),
-			
-			...sem_post(nobjects, 0, 1),
-		]
-	},
-},
-
+threads,
 o=>{
-	o.memory[11] = 1; // buffer length
+	o.memory[11] = 2; // buffer length
 	o.memory[2]  = o.memory[3]  = 4;
 	o.memory[12] = o.memory[13] = 14;
 	o.memory[22] = o.memory[23] = 24;
